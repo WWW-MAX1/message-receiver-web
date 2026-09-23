c39181aedd6bf65a1a5d106170647922bde55a41
WWW-MAX1 <1215209546@qq.com>
2026-09-23 21:39:26 +0800

    Publish phone message web: sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='message-receiver-v1';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./style.css','./app.js','./protocol.js','./icon.svg','./manifest.webmanifest'])));self.skipWaiting();});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET'||new URL(e.request.url).origin!==self.location.origin)return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
+
